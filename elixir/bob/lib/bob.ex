6d946f176ba82ec7b90e315bebd1c50822cacae9
bradhanks <hanks.brad@gmail.com>
2020-04-20 00:08:05 -0600

    finished bob.ex version 1.0

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,18 +1,27 @@
-
-# ? -> 'Sure.'
-
-# ! or all caps (any letters) -> 'Whoa, chill out!' .
-
-# all cps and ? -> 'Calm down, I know what I'm doing!'
-
-# \W 'Fine. Be that way!'
-
-# 'Whatever.' to anything else.
-
 defmodule Bob do
-  def hey(input) do
-    cond do
-      true -> raise "Your implementation goes here"
-    end
+  @messages       [
+                    {~r/^(?(?=\b[A-Z]+\b).*)\?$/, "Calm down, I know what I'm doing!"},
+                    {~r/^.*\?$/, "Sure."},
+                    {~r/^[\W]*$/, "Fine. Be that way!"},
+                    {~r/^\b[A-Z]+\b.*$/, "Whoa, chill out!"},
+                    {~r/^[^a-z]+!$/, "Whoa, chill out!"},
+                    {~r/^[^a-z0-9]+$/, "Whoa, chill out!"},
+                    {~r/^.*/, "Whatever."}
+
+                  ]
+
+  def hey(input), do: reply(input)
+
+  def reply(msg) do
+    msg |> String.trim |> parse(@messages, "", _match_found = false)
   end
+
+  defp parse(_msg, _messages, reply, true) do
+    reply
+  end
+
+  defp parse(msg, [{regex, response} | tail], _reply, _match_found) do
+    parse(msg, tail, response, Regex.match?(regex, msg))
+  end
+
 end
