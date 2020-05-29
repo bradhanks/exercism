9880a7d73ede543ed157d7fb25893de80904b1ed
bradhanks <hanks.brad@gmail.com>
2020-05-29 16:27:01 -0600

    pangram done

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,5 @@
+import re
 def is_pangram(sentence):
-    pass
+    sentence = sentence.lower()
+    inputset = [item for item in set(re.split(r'(\B|\b|\s)', sentence)) if re.compile(r'[a-z]').match(item)]
+    return len(inputset) == 26   
