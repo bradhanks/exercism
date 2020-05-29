eec903894679b848d5c5c1db61716f65f590d83d
bradhanks <hanks.brad@gmail.com>
2020-05-29 15:53:06 -0600

    finished matrix, starting pangram

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,16 @@
+import sys
+sys.path.append("numpy_path")
+import numpy as np
+
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix = matrix_string
-        self.rows = matrix_string.count('\n') + 1
-        self.columns = matrix_string.index('\n', 0, len(matrix_string))
-
+        self.matrix = [[int(j) for j in i.split()] for i in matrix_string.splitlines()]
+    
     def row(self, index):
-        return self.matrix
+        return self.matrix[index-1]
 
     def column(self, index):
+        column = []
+        [[column.append(self.matrix[i][index-1]) ] for i in range(len(self.matrix))]
         
+        return column
