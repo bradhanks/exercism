8555414cb9b157b2a1f16466dd99505d95a02306
bradhanks <hanks.brad@gmail.com>
2020-07-07 04:31:18 -0600

    destructuring, spread operator, fine tuning

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,12 +1,10 @@
 export const rows = (num, x = [], count = 0) => {
-  if(num == 0) {return x;} 
-  x[count] = [];
-  for(let i = 0; x[count].length < count; i++){
-    x[count].push(
-                  (x[count-1][i-1] | 0) + 
-                  (x[count-1][i]   | 0)
-                 );
-  }
-  x[count].push(1);
-  return rows(num - 1, x, count + 1);
+    if(num === 0) {return x;} 
+    let row = [];
+    for(let i = 0; row.length < count; i++){
+      row = [...row, (x[count-1][i-1] || 0) + (x[count-1][i] || 0)];
+    }
+    x = [...x, [...row, 1]]
+    return rows(num - 1, x, count + 1);
+
 };
