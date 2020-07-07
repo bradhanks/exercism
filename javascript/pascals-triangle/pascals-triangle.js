e6bc387f2245237587ba105ed00fe5bdc95492ef
bradhanks <hanks.brad@gmail.com>
2020-07-07 05:34:52 -0600

    map refactoring

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,10 +1,7 @@
-export const rows = (num, x = [], count = 0) => {
-    if(num === 0) {return x;} 
-    let row = [];
-    for(let i = 0; row.length < count; i++){
-      row = [...row, (x[count-1][i-1] || 0) + (x[count-1][i] || 0)];
-    }
+export const rows = (num, x = [[1]], count = 0) => {
+    if(num === 0) { return []; } 
+    if(num === 1) { return x;  }
+    const [...row] = x[count].map((item, index, arr) => (arr[index-1] || 0) + (arr[index] || 0));
     x = [...x, [...row, 1]]
     return rows(num - 1, x, count + 1);
-
 };
