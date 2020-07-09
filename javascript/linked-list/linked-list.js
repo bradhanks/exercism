71c28a7c7ece51482d5ee5a91334cc3de97e849b
bradhanks <hanks.brad@gmail.com>
2020-07-09 16:12:52 -0600

    accidentally changed this file and need to put it back to earlier verison: git checkout [commit hash] [file path] learning lots though

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,94 +1,53 @@
-export class Node {
-  constructor(data) {
-    this.data = data;
+class Node {
+  constructor(value) {
+    this.value = value;
     this.next = null;
     this.prev = null;
   }
 }
-
 const head = Symbol("head");
 const tail = Symbol("tail");
 export class LinkedList {
   constructor() {
-    this[head] = null;
-    this[tail] = null;
-    this.size = 0;
+    this[head] = {value: null};
+    this[tail] = {value: null};
+    this[head].next = this[tail];
+    this[tail].prev = this[head];
+    this.length = 0;
   }
-
-  push(data) {
-    this.size += 1;
-    // create a new node
-    const newNode = new Node(data);
-    // create head for objects with nothing stored in the linked list
-    if(this[tail]) {
-      // existing tail is new previous node
-    this[tail].next = newNode;
-    newNode.prev = this[tail];
-    // newNode is new tail
-    this[tail] = newNode; 
-    } else {
-      this[tail] = newNode;
+  // Helper methods
+  isNotEmpty(node)  { return node.value !== null;  }
+  createNode(value) { return new Node(value); }
+  addNode(prevNode, newNode, nextNode)  {
+    newNode.prev = prevNode;
+    newNode.next = nextNode;
+    prevNode.next = nextNode.prev =  newNode;
+    this.length += 1;
+  }
+  removeNode(node)  {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    this.length -= 1;
+  }
+  removeNodeCheck(node, returnValue = true) {
+    if(this.isNotEmpty(node)) {
+      this.removeNode(node);
+      if(returnValue) { return node.value;  }
     }
-    if(this[head] == null) { this[head] = newNode; }
   }
-
-  pop() {
-    this.size = Math.max(0, this.size - 1);
-
-    //pull of last value of the list
-    const popNode = this[tail];
-    //Update tail
-    if(this[tail]) { 
-      this[tail] = this[tail].prev; 
-    } else {
-      this[tail] = this[head];
-    }
-    if(this[head] == null) { this[head] = this[tail]; }
-
-    return popNode ? popNode.data : popNode;
+  findNodeByValue(value)  {
+    let searchNode = this[head];
+    do {
+      searchNode = searchNode.next;
+      if(searchNode.value === value)  { return searchNode;  }
+    } while(searchNode.next);
+    return {value: null};
   }
-
-  shift() {
-    this.size = Math.max(0, this.size - 1);
-    //pull of head node off the list
-    const shiftNode = this[head];
-    if(this[head]) { this[head] = this[head].next; }
-    if(this[tail] == null) { this[tail] = this[head]; }
-    return shiftNode ? shiftNode.data : shiftNode;
-  }
-
-  unshift(data) {
-    this.size += 1;
-    //put new node at the head
-    const unshiftNode = new Node(data);
-    
-    if(this[head]) { 
-      this[head].prev = unshiftNode;
-      unshiftNode.next = this[head];
-      this[head] = unshiftNode;
-    } else {
-      this[head] = unshiftNode;
-    }
-    if(this[tail] == null) { this[tail] = unshiftNode; }
-  }
-
-  delete(data) {
-    //remove reference
-    let search = this[head];
-    let node = {};
-    do{
-      if(search.data === data){
-        node = search;
-        this.size = Math.max(this.size - 1);
-        break;
-      }
-      search = search.next;
-    }while(search)
-    if(node.prev) { node.prev.next = node.next; } else { node.next = this[head]; }
-    if(node.next) { node.next.prev = node.prev; } else { node.prev = this[tail]; }
-  }
-
-  count() {
-    return this.size;
-  }
+  // Methods
+  count()           { return this.length;                                                 }
+  delete(value)     { this.removeNodeCheck(this.findNodeByValue(value), false);           }
+  pop()             { return this.removeNodeCheck(this[tail].prev);                       }
+  push(value)       { this.addNode(this[tail].prev, this.createNode(value), this[tail]);  }
+  shift()           { return this.removeNodeCheck(this[head].next);                       }
+  unshift(value)    { this.addNode(this[head], this.createNode(value), this[head].next);  }
 }
