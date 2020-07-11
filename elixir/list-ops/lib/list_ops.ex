69f5eb66b0823e11a10ce392e95710465e744ced
bradhanks <hanks.brad@gmail.com>
2020-07-10 21:02:15 -0600

    refactoring list operations js

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -1,42 +1,21 @@
 defmodule ListOps do
-  # Please don't use any external modules (especially List or Enum) in your
-  # implementation. The point of this exercise is to create these basic
-  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
-  # for adding numbers), but please do not use Kernel functions for Lists like
-  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
-
-  @spec count(list) :: non_neg_integer
   def count(list \\ [], i \\ 0)
   def count([],i), do: i
-  def count([ _h | t ], i), do: count(t, i + 1)
-
-  @spec reverse(list, revlist) :: list
-  def reverse(list \\ [], revlist \\ [])
-  def reverse([], revlist), do: revlist
-  def reverse([ h | t ], revlist), do: reverse(t, append(revlist, h))
-  end
-
-  @spec map(list, (any -> any)) :: list
-  def map(l \\ [], f \\ fn x -> x end, newl \\ [])
-  def map([], f, newl), do: newl
-  def map([h | t ], f, newl), do: map(t, f, append(newl, f.(h))
-
+  def count([_h | t], i), do: count(t, i+1)
+  def reverse(list), do: list |> reduce([], &[&1| &2])
+  def map(list, fun), do: list |> reverse() |> reduce([], &[fun.(&1) | &2])
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l \\ [], f \\ fn x -> x%1 == 0 end)
-  def filter([], f, newl), do: newl
-  def filter([h | t ], f, newl), do: map(t, f, append(newl, f.(h))
-
-  @type acc :: any
-  @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(list \\ [], fn(x, fn ) ->
-  def reduce([h | t], acc, acc.(h, acc.(t))) do
+  def filter(list \\ [], f \\ fn x -> x == 0 end, newlist \\ [])
+  def filter([], _f, newlist), do: reverse(newlist)
+  def filter([h | t ], f, newlist) do
+    if f.(h) do
+      filter(t, f, [h | newlist])
+    else
+      filter(t, f, newlist)
+    end
   end
-
-  @spec append(list, list) :: list
-  def append(a, b) do
-  end
-
-  @spec concat([[any]]) :: [any]
-  def concat(ll) do
-  end
+  def reduce([], acc, _fun), do: acc
+  def reduce([h | t], acc, fun), do: h |> fun.(acc) |> (&(reduce(t, &1, fun))).()
+  def append(a, b), do: a |> reverse |> reduce(b, &[&1 | &2])
+  def concat(a), do: a |> reverse |> reduce([], &append(&1, &2))
 end
